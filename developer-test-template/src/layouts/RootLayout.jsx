d1be24180f379f8115fb429ab635dd714da5b916
DESKTOP-BETG49P\A <fishwwww0516@naver.com>
2026-02-27 16:49:07 +0900

    feature: connect type api

--- developer-test-template/src/layouts/RootLayout.jsx
+++ developer-test-template/src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function RootLayout() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-transparent p-6">
+    <div className="flex min-h-screen items-center justify-center bg-[linear-gradient(180deg,var(--color-bg-default)_0%,#ffffff_100%)]">
       <div className="w-full max-w-[520px]">
         <Outlet />
       </div>
