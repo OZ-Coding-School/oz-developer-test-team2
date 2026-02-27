361979e7c1beb66b906d9f55d70970ad367be4a4
ohseungyeon <alstmd9902@naver.com>
2026-02-27 18:02:03 +0900

    refactor: option button style update and filename fix

--- developer-test-template/src/components/common/index.js
+++ developer-test-template/src/components/common/index.js
@@ -1,5 +1,5 @@
-export { default as Card } from './Card';
-export { default as Button } from './Button';
-export { default as CharacterIcon } from './character';
-export { default as Progressbar } from './Progressbar';
 export { default as badge } from './badge';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as CharacterIcon } from './character';
+export { default as ProgressBar } from './Progressbar';
