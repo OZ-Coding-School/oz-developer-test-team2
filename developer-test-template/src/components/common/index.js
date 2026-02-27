b7aee7f6d0f2c57b36b4c7a8ad027eb66723f192
DESKTOP-BETG49P\A <fishwwww0516@naver.com>
2026-02-27 14:45:50 +0900

    fix: bugfix

--- developer-test-template/src/components/common/index.js
+++ developer-test-template/src/components/common/index.js
@@ -1,5 +1,4 @@
 export { default as Card } from './Card';
 export { default as Button } from './Button';
-export { default as CharacterIcon } from './CharacterIcon';
 export { default as Progressbar } from './Progressbar';
 export { default as badge } from './badge';
