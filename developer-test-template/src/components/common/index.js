3459730732462fccf9fd5f4ba27f5f5cd89851a6
ohseungyeon <alstmd9902@naver.com>
2026-02-27 14:33:27 +0900

    refactor: apply barrel pattern to common components (#27)
    Closes #26

---
+++ developer-test-template/src/components/common/index.js
@@ -0,0 +1,5 @@
+export { default as Badge } from './badge';
+export { default as Button } from './button';
+export { default as Card } from './card';
+export { default as CharacterIcon } from './character';
+export { default as ProgressBar } from './progressbar';
