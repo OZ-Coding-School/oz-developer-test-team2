dec937edf646603f451de80729af5f4de4401265
ohseungyeon <alstmd9902@naver.com>
2026-03-04 13:00:06 +0900

    refactor: move API baseURL to env and modularize questions service (#41) (#42)
    Closes #41

--- developer-test-template/src/hooks/queries/index.js
+++ developer-test-template/src/hooks/queries/index.js
@@ -1,15 +1,10 @@
+import { getQuestionsByType } from '@/api/services/questions';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-//api 호출
-export async function getQuestions() {
-  const { data } = await axios.get('http://localhost:3000/api/questions');
-  return data;
-}
 
 export const useQuestion = () => {
   return useQuery({
     queryKey: ['questions'],
-    queryFn: getQuestions,
+    queryFn: getQuestionsByType,
     staleTime: 5000,
   });
 };
